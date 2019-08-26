8bbfd8733c0214fd25ec6eb1e2725d9d1c542b03
eabgaryan <garinich.7@gmail.com>
2019-08-26 17:10:22 +0600

    get and parse caption

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -1,34 +1,41 @@
 import { youtubeApiKey } from '../../keys'
 
-const vidoyId = 'Yocja_N5s1IYocja_N5s1I';
-const captionId = 'TLlfC6fxsMkNxTGbh5uc_PSHfRrtHqFh';
-const captionId2 = 'iA80G5cFenkN7wCVdVBQqUh2FiJq37-mJCHN-PM7MGc=';
+const vidoyId = 'Yocja_N5s1I';
+const captionId1 = 'TLlfC6fxsMkMNiXR_JDSyxU4pYV9Aehn';
+const captionId2 = 'iA80G5cFenkN7wCVdVBQqUh2FiJq37-mJCHN-PM7MGc='; // auto generated
 
 
 
-console.log('${youtubeApiKey}', youtubeApiKey)
-'GET https://www.googleapis.com/youtube/v3/captions?part=id&videoId=Yocja_N5s1I&key={YOUR_API_KEY}';
-const captureUrl = `https://www.googleapis.com/youtube/v3/captions/iA80G5cFenkN7wCVdVBQqUh2FiJq37-mJCHN-PM7MGc%3D?key=${youtubeApiKey}`;
-const chanelsUrl = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails';
-const capturesExampleUrl = `https://content.googleapis.com/youtube/v3/captions?part=snippet&videoId=Yocja_N5s1I&key=${youtubeApiKey}`
+const capturesListUrl = `https://content.googleapis.com/youtube/v3/captions?part=snippet&videoId=${vidoyId}&key=${youtubeApiKey}`
+const captureUrl = `https://www.googleapis.com/youtube/v3/captions/${captionId1}?key=${youtubeApiKey}&tfmt=sbv`;
 
 
-const theUrl = `https://www.googleapis.com/youtube/v3/captions/iA80G5cFenkN7wCVdVBQqUh2FiJq37-mJCHN-PM7MGc=?key=${youtubeApiKey}`;
-
+// We are serverless, so we need to asc users connect their account???
 chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-  console.log('token', token);
-
-  // fetch(theUrl, {
-  //   withCredentials: true,
-  //   headers: new Headers({
-  //     Authorization: 'Bearer ' + token,
-  //     Accept: 'application/json',
-  //   }),
-  // })
-  //   .then(r => r.text())
-  //   .then(r => console.log('fetch r',r))
-  //   .catch(e => console.log('e',e));
+  fetch(captureUrl, {
+    headers: new Headers({
+      Authorization: 'Bearer ' + token,
+      Accept: 'application/json',
+    }),
+  })
+    .then(r => r.text())
+    .then(r => parseSbvCaption(r))
+    .catch(e => console.log('e',e));
 });
+
+function parseSbvCaption(caption) {
+  let res = caption.split('\n\n')
+    .map( sub => sub.match(/(\d:\d\d:\d\d\.\d\d\d),(\d:\d\d:\d\d\.\d\d\d)\s*((.|\s)*)/))
+    .filter( matchRes => matchRes)
+    .map(matchRes => ({
+      from: matchRes[1],
+      to: matchRes[2],
+      text: matchRes[3].trim().replace(/\s+/g, ' '),
+    }))
+  ;
+  console.log(typeof res, Array.isArray(res), res);
+  return res;
+};
 
 
 export default 21
